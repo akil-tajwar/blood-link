44ff56ac5fe7a5d28343d65781b598731090bae5
Akil Tajwar <akiltajwar@gmail.com>
2023-10-05 02:23:40 +0600

    adding footer

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Components/Banner';
 import Motivation from './Components/Motivation';
+import Highlighting from './Components/Highlighting';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <Motivation/>
+            <Highlighting/>
         </div>
     );
 };
--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -11,14 +11,14 @@ const Navbar = () => {
     )
     return (
         <div className='fixed w-full'>
-            <div className='bg-black bg-opacity-5 w-11/12 lg:w-3/4 px-5 mx-auto rounded-lg mt-5'>
+            <div className='bg-[#f04d4d] backdrop-blur text-white w-11/12 lg:w-3/4 px-5 mx-auto rounded-lg mt-5'>
                 <div className='flex justify-between items-center'>
                     <div className='flex items-center'>
                         <div className="dropdown lg:hidden">
                             <label tabIndex={0} className="btn btn-ghost btn-circle">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                             </label>
-                            <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                            <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content text-black bg-base-100 rounded-box w-52">
                                 {menu}
                             </ul>
                         </div>
@@ -27,7 +27,7 @@ const Navbar = () => {
                     <div className="hidden lg:block">{menu}</div>
                     <div className='flex gap-5 items-center'>
                         <div className="">
-                            <input type="text" placeholder="Search" className="w-24 border p-2 rounded-lg md:w-auto" />
+                            <input type="text" placeholder="Search" className="w-24 border px-2 py-1 rounded-lg md:w-auto" />
                         </div>
                         <div className="dropdown dropdown-end">
                             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
@@ -35,7 +35,7 @@ const Navbar = () => {
                                     <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                                 </div>
                             </label>
-                            <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                            <ul tabIndex={0} className="mt-3 z-[1] text-black p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
                                 <li>
                                     <a className="justify-between">
                                         Profile
