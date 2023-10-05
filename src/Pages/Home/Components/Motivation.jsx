ab047e2c4012fc98d7ffc4f4f4e25deb53312ef1
Akil Tajwar <akiltajwar@gmail.com>
2023-10-05 13:19:26 +0600

    adding highlighting section

--- src/Pages/Home/Components/Motivation.jsx
+++ src/Pages/Home/Components/Motivation.jsx
@@ -8,13 +8,13 @@ const Motivation = () => {
                     <img className='rounded-lg' src="donation1.jpg" alt="" />
                 </div>
                 <div className='col-span-2'>
-                    <h3 className='text-3xl pb-5 font-semibold'>Save a life by just giving us 10 minutes</h3>
+                    <h3 className='text-3xl pb-5 text-[#f04d4d] font-semibold'>Save a life by just giving us 10 minutes</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, cupiditate deserunt dignissimos officiis architecto blanditiis repellendus iure odio consequuntur natus iusto quidem dolores. Nemo, corporis hic! Nemo amet, voluptates laborum vitae praesentium id. Eos velit, nisi adipisci reiciendis consequatur vitae. Possimus vel neque deleniti rerum provident odio dolores ex voluptatibus, eius saepe. Expedita possimus voluptates aut quo tempore nulla perspiciatis consequatur quisquam minus accusantium molestiae dolore amet corrupti quasi eligendi ab, iure, beatae similique repellat animi earum, ut laudantium rerum? Repudiandae eos deserunt natus quos, dolorum culpa assumenda impedit in ad mollitia ratione quibusdam accusamus similique eaque consequuntur dolore doloremque?</p>
                 </div>
             </div>
             <div className='grid grid-cols-3 gap-5 pt-20'>
                 <div className='col-span-2'>
-                    <h3 className='text-3xl pb-5 font-semibold'>Together we can make difference</h3>
+                    <h3 className='text-3xl pb-5 text-[#f04d4d] font-semibold'>Together we can make difference</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, cupiditate deserunt dignissimos officiis architecto blanditiis repellendus iure odio consequuntur natus iusto quidem dolores. Nemo, corporis hic! Nemo amet, voluptates laborum vitae praesentium id. Eos velit, nisi adipisci reiciendis consequatur vitae. Possimus vel neque deleniti rerum provident odio dolores ex voluptatibus, eius saepe. Expedita possimus voluptates aut quo tempore nulla perspiciatis consequatur quisquam minus accusantium molestiae dolore amet corrupti quasi eligendi ab, iure, beatae similique repellat animi earum, ut laudantium rerum? Repudiandae eos deserunt natus quos, dolorum culpa assumenda impedit in ad mollitia ratione quibusdam accusamus similique eaque consequuntur dolore doloremque?</p>
                 </div>
                 <div>
